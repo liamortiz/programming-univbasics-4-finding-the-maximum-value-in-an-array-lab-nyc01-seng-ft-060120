117dcdade733de462db3741ea0878f69af7d9e4c
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 16:26:24 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,4 +2,8 @@ def find_max_value(array)
   max = 0
   counter = 0
   while counter < array.size do
+    max = array[counter] > max ? array[counter] : max
+    counter += 1
+  end
+  return max
 end
